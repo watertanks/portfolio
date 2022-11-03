ec841d1bf4358b7369bc2f4a3780070e86b15da5
watertanks <watertank0928@gmail.com>
2022-11-03 19:02:50 +0900

    hjh

---
+++ js/index.js
@@ -0,0 +1,20 @@
+
+
+
+
+
+
+    let passReg = /hello world/
+    var p1Link = './port1.html'
+$('.btn').on('click',function(){
+    event.preventDefault()
+    if( !passReg.test($('input[name=passWord]').val()) ){
+        alert('접속코드가 틀렸습니다.')
+        $('input[name=passWord]').focus();
+        return;
+    }else{
+        // console.log(0)
+        window.location.href = p1Link;
+    }
+})
+
