7b805065cbf1780d3210091822a40d86e0c3ab96
watertanks <watertank0928@gmail.com>
2022-11-07 10:11:29 +0900

    finally

--- js/p7.js
+++ js/p7.js
@@ -1,5 +1,18 @@
 
+var p8Link = './port8.html'
 var p6Link = './port6.html'
+
+$(document).keyup(function(event){
+    if(event.which === 13) {
+        window.location.href = p8Link;
+    }
+})
+
+$(document).keyup(function(event){
+    if(event.which === 39) {
+        window.location.href = p8Link;
+    }
+})
 
 $(document).keyup(function(event){
     if(event.which === 37) {
---
+++ js/p8.js
@@ -0,0 +1,8 @@
+
+var p7Link = './port7.html'
+
+$(document).keyup(function(event){
+    if(event.which === 37) {
+        window.location.href = p7Link;
+    }
+})
